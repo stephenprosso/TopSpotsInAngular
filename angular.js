667b660e0bbf5a2fbb568c318738437f6b7c5199
Stephen Rosso <stephenprosso@gmail.com>
2017-03-28 16:56:06 -0700

    TopSpots with API

--- angular.js
+++ angular.js
@@ -4,10 +4,22 @@ angular.module('topSpots').controller("HomeController", HomeController);
 
 function HomeController($http) {
     var vm = this;
-
+    var apiUrl = "http://localhost:49832/api/getTopSpot";
     $http
-    .get("index.json")
-    .then(function(response){
+        .get(apiUrl)
+        .then(function(response) {
             vm.places = response.data;
         });
+
+    vm.AddNewSpot = function AddNewSpot() {
+        var newTopSpot = {
+            Name: vm.newSpotName,
+            Description: vm.newSpotDescription,
+        };
+        $http
+            .post(apiUrl, newTopSpot)
+            .then(function(response) {
+                vm.places = response.data;
+            });
+    }
 }
